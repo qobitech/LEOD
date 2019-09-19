71de158fe39be0f81e8ea286e06b1d9ebc8030b9
Qobi <qobitek@gmail.com>
2019-09-19 09:59:08 +0100

    .

--- js/auth.js
+++ js/auth.js
@@ -1,11 +1,10 @@
 
 
 const signupform = document.getElementById('reg');
-
+message = document.querySelector('.message');
 signupform.addEventListener('submit',(e)=>{
     e.preventDefault();
    
-
     //get user info
     const email = document.getElementById('email-signup').value;
     const password = document.getElementById('password').value;
@@ -17,8 +16,19 @@ signupform.addEventListener('submit',(e)=>{
         //signup successful
         $('.nav-tabs a[href="#profile-classic"]').tab('show')
     })
-
-
+    setTimeout(logout,2000);
+    signupform.reset();
+    message.innerHTML=`REGISTRATION SUCCESSFUL`;
 
 
 })
+
+function logout(){
+    const lout = document.querySelector('');
+    lout.addEventListener('click', (e)=>{
+        e.preventDefault();
+        auth.signOut().then(res=>{
+            console.log(res.user);
+        })
+    })
+}

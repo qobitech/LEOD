9f279881038d4f20e71348789de05eae939a3bb8
Edekobi Frank <qobitek@gmail.com>
2019-09-19 06:11:59 +0100

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -2,7 +2,7 @@ const signupform = document.querySelector('reg');
 signup.addEventListener('submit',(e)=>{
     e.preventDefault();
 
-
+    alert('we don show');
     //get user info
     const email = signupform['email-signup'].value;
     const password = signupform['password'].value;

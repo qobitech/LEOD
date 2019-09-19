a0d6f64a16adfec2cd6b62a276b73cc518db0dd1
Qobi <qobitek@gmail.com>
2019-09-19 22:08:51 +0100

    .

--- js/auth.js
+++ js/auth.js
@@ -18,17 +18,59 @@ signupform.addEventListener('submit',(e)=>{
     })
     setTimeout(logout,2000);
     signupform.reset();
-    message.innerHTML=`REGISTRATION SUCCESSFUL`;
+    // message.innerHTML=`REGISTRATION SUCCESSFUL`;
     
+    firebase.auth().onAuthStateChanged(function(user){
+        if(user){
+            //User is signed in
+
+            var user = firebase.auth().currentUser;
+
+            if(user != null){
+                var email_id = user.email;
+                var email_verified = user.emailVerified;
+                console.log('Welcome User :' + email_id +
+                '<br/>Verified : ' + email_verified)
+            }
+        }
+    })
 
 })
+
+
+
+// function emailverification(){
+//     var actionCodeSettings = {
+//         url: 'https://qobitech.github.io/LEOD/?email=' + firebase.auth().currentUser.email,
+//         iOS: {
+//           bundleId: 'com.example.ios'
+//         },
+//         android: {
+//           packageName: 'com.example.android',
+//           installApp: true,
+//           minimumVersion: '12'
+//         },
+//         handleCodeInApp: false,
+//         // When multiple custom dynamic link domains are defined, specify which
+//         // one to use.
+//         dynamicLinkDomain: "example.page.link"
+//       };
+//       firebase.auth().currentUser.sendEmailVerification(actionCodeSettings)
+//         .then(function() {
+//           // Verification email sent.
+//         })
+//         .catch(function(error) {
+//           // Error occurred. Inspect error.code.
+//         });
+// }
+
 
 function logout(){
     // const lout = document.querySelector('');
     // lout.addEventListener('click', (e)=>{
         // e.preventDefault();
         auth.signOut().then(res=>{
-            console.log(res);
+            console.log('user signed out');
         })
     // })
 }

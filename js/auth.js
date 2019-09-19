33415f580a67dead3cc76940d1ab94a9ef091bc6
Qobi <qobitek@gmail.com>
2019-09-19 03:47:05 +0100

    .

--- js/auth.js
+++ js/auth.js
@@ -4,7 +4,7 @@ const signupform = document.getElementById('reg');
 
 signupform.addEventListener('submit',(e)=>{
     e.preventDefault();
-    alert('submitted');
+   
 
     //get user info
     const email = signupform['email-signup'].value;
--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+alert('submitted');
+
 // document.querySelector('#logForm').addEventListener('submit',(event)=>{
 //     event.preventDefault();
 // })

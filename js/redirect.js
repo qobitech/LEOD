bd72161b375ced28f5b7c5140cb37e81e0b39750
Qobi <qobitek@gmail.com>
2019-09-20 07:59:08 +0100

    .

--- js/redirect.js
+++ js/redirect.js
@@ -6,8 +6,14 @@ function login(){
     window.location.href = "https://qobitech.github.io/LEOD/";
 }
 
+function logout(){
+    auth.signOut().then(function(){
+        window.location.replace("https://qobitech.github.io/LEOD");
+    })
+}
+
 $('#signout').on('click',()=>{
-    // logout();
+    logout();
     console.log('signout');
 })
 

7a5a7d0173f989f9bba91781ec98cd11d937981c
Qobi <qobitek@gmail.com>
2019-09-20 06:19:37 +0100

    .

--- js/auth.js
+++ js/auth.js
@@ -67,7 +67,7 @@ loginform.addEventListener('submit',(e)=>{
           firebase.auth().currentUser.email, 
           password
         )
-    ).then(res=>{console.log(res.user)});
+    ).then(res=>{console.log(res)});
 
     // firebase.auth().onAuthStateChanged(function(user){
     //     if(user){

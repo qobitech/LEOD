69d3a6e74dfda2e8deef37fb2f552aebc10f8d17
Qobi <qobitek@gmail.com>
2019-09-20 05:26:46 +0100

    .

---
+++ js/redirect.js
@@ -0,0 +1,10 @@
+if(window.location.href === "https://qobitech.github.io/LEOD/verifyemail.html"){
+    setTimeout(login,4000);
+} 
+
+
+
+
+function login(){
+    window.location.href = "https://qobitech.github.io/LEOD/";
+}

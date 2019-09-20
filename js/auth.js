f242843a5c2c415232d9e460f42381f2611772a4
Qobi <qobitek@gmail.com>
2019-09-20 06:10:27 +0100

    .

--- js/auth.js
+++ js/auth.js
@@ -45,6 +45,12 @@ signupform.addEventListener('submit',(e)=>{
 
 const loginform = document.getElementById('logForm');
 
+
+auth.onAuthStateChanged(user => {
+
+})
+
+
 //Login
 loginform.addEventListener('submit',(e)=>{
     e.preventDefault();
@@ -52,26 +58,33 @@ loginform.addEventListener('submit',(e)=>{
     const email = document.getElementById('logmail').value;
     const password = document.getElementById('logpass').value;
     firebase.auth().signInWithEmailAndPassword(email,password).then(cred => {
-        console.log(cred.user)
-    })
-    firebase.auth().onAuthStateChanged(function(user){
-        if(user){
-            //User is signed in
-            var user = firebase.auth().currentUser;
+        // console.log(cred.user)
 
-            if(user != null){
-                var email_id = user.email;
-                var email_verified = user.emailVerified;
-                console.log('Welcome User :' + email_id +
-                '<br/>Verified : ' + email_verified)
-                if(email_verified === true){
-                    window.location.replace("https://qobitech.github.io/LEOD/loginsuccess.html");
-                }else{
-                    setTimeout(logout,2000);
-                }
-            }
-        }
     })
+
+    firebase.auth().currentUser.reauthenticateWithCredential(
+        firebase.auth.EmailAuthProvider.credential(
+          firebase.auth().currentUser.email, 
+          password
+        )
+    ).then(res=>{console.log(res)});
+
+    // firebase.auth().onAuthStateChanged(function(user){
+    //     if(user){
+    //         //User is signed in
+    //         var user = firebase.auth().currentUser;
+
+    //         if(user != null){
+    //             var email_id = user.email;
+    //             var email_verified = user.emailVerified;
+    //             if(email_verified === true){
+    //                 window.location.replace("https://qobitech.github.io/LEOD/loginsuccess.html");
+    //             }else{
+    //                 setTimeout(logout,2000);
+    //             }
+    //         }
+    //     }
+    // })
 })
 
 function send_verification(){

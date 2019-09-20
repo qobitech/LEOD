1825f0ac234c8a8a8921f0498c5fe7750936d7c1
Qobi <qobitek@gmail.com>
2019-09-20 05:03:09 +0100

    .

--- js/redirect.js
+++ js/redirect.js
@@ -1,5 +1,5 @@
 if(window.location.href !== "https://qobitech.github.io/LEOD/"){
-    window.location.href.replace("https://qobitech.github.io/LEOD/");
+    window.location.replace("https://qobitech.github.io/LEOD/");
     // console.log('redirect');
 }
 
